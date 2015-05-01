6d9601467310657919402aff8a98bb19deb2cac5
unknown <ajoseps@gmail.com>
2015-04-30 23:48:42 -0700

    added more text views to show locations, fixed spinner listener

--- app/src/main/java/com/geofencing/geofence/MainActivity.java
+++ app/src/main/java/com/geofencing/geofence/MainActivity.java
@@ -14,7 +14,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-
+import com.getpebble.android.kit.Constants;
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+import java.util.UUID;
 public class MainActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
     //DEBUGGING
     private static final String TAG = "GEOFENCING";
@@ -39,11 +42,10 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
         @Override
         public void onLocationChanged(final Location location) {
             mLocation = location;
-            TextView currentLocationTextView = (TextView) findViewById(R.id.currentLocationTextView);
-            currentLocationTextView.setText(location.getLatitude() + " " + location.getLongitude());
 
             // Checks to see if out of range of fence
             withinFence();
+            sendToPebble();
         }
 
         @Override
@@ -61,6 +63,15 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
     };
 
 
+    //////////////////////////////////////////////////////////////////
+    // Pebble Variables //////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+    private static final UUID GEOFENCE_APP_UUID = UUID.fromString("667d207f-8e0a-4994-b348-853d65987b1d");
+    private static final int
+            STATUS_KEY = 0,
+            MESSAGE_KEY = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,10 +86,11 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        // Apply Listener to Spinner
+        spinner.setOnItemSelectedListener(this);
 
         // Initilaizes Android Location Manager
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
                 LOCATION_REFRESH_DISTANCE, mLocationListener);
     }
@@ -104,27 +116,52 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
                                int pos, long id) {
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
-        radiusOfFence = (float) parent.getItemAtPosition(pos);
+        radiusOfFence = Float.parseFloat((String)parent.getItemAtPosition(pos));
+        Log.v(TAG, "SPINNER ITEM SELECTED: " + Float.toString(radiusOfFence));
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
+        radiusOfFence = (float) parent.getItemAtPosition(0);
+        Log.v(TAG, "SPINNER ITEM DEFAULT: " + Float.toString(radiusOfFence));
     }
 
     // onClick listener for setLocation Button
     // Sets the current center of GeoFence to the current location
     public void setLocationButtonClick(View view){
         centerOfFence = new Location(mLocation);
-        Log.v(TAG, "SET CENTER OF FENCE!");
+        TextView currentCenterTextView = (TextView) findViewById(R.id.currentCenterTextView);
+        currentCenterTextView .setText("CENTER OF FENCE SET: \n" + getCurrentLocationAsString());
+        setLocationTextViewText(getCurrentLocationAsString());
+        Log.v(TAG,"CENTER LOCATION SET");
     }
 
     // Checks if user is still within the fence depending on fence range selected
     private void withinFence(){
         if(mLocation != null && centerOfFence != null) {
             if (mLocation.distanceTo(centerOfFence) > radiusOfFence) {
-                TextView currentLocationTextView = (TextView) findViewById(R.id.currentLocationTextView);
-                currentLocationTextView.setText("!!EXITED FENCE!!");
+                setLocationTextViewText("!!EXITED FENCE!!\n" + getCurrentLocationAsString());
+            }
+            else{
+                setLocationTextViewText(getCurrentLocationAsString());
             }
         }
+    }
+
+    // Send a message to Pebble
+    public void sendToPebble() {
+        PebbleDictionary dict = new PebbleDictionary();
+        dict.addString(MESSAGE_KEY, "EXITED FENCE");
+        PebbleKit.sendDataToPebbleWithTransactionId(getApplicationContext(), GEOFENCE_APP_UUID, dict, 255);
+    }
+
+    // HELPER METHODS
+    private void setLocationTextViewText(String text){
+        TextView currentLocationTextView = (TextView) findViewById(R.id.currentLocationTextView);
+        currentLocationTextView.setText(text);
+    }
+
+    private String getCurrentLocationAsString(){
+        return mLocation.getLatitude() + " " + mLocation.getLongitude();
     }
 }
