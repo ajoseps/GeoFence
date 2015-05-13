eacd1a39c34220066864e1f8a06c09ccfb1d93fe
Arun Joseph <ajoseps@gmail.com>
2015-05-12 22:03:24 -0700

    Adding Pebble Code

---
+++ src/app_message.c
@@ -0,0 +1,120 @@
+#include <pebble.h>
+
+Window *window;	
+static TextLayer *s_output_layer;;
+	
+// Key values for AppMessage Dictionary
+enum {
+	STATUS_KEY = 0,	
+	MESSAGE_KEY = 1,
+  EXIT_KEY = 2
+};
+
+// Write message to buffer & send
+void send_message(void){
+	DictionaryIterator *iter;
+	
+	app_message_outbox_begin(&iter);
+	dict_write_uint8(iter, STATUS_KEY, 0x1);
+	
+	dict_write_end(iter);
+  	app_message_outbox_send();
+}
+
+// Sets the text layer of the pebble to a certain text message
+static void set_text_layer_text(Window *window, char* text){
+  text_layer_set_text(s_output_layer, text);
+}
+
+// Called when a message is received from PebbleKitJS
+static void in_received_handler(DictionaryIterator *received, void *context) {
+	Tuple *tuple;
+	
+	tuple = dict_find(received, STATUS_KEY);
+	if(tuple) {
+	}
+	
+	tuple = dict_find(received, MESSAGE_KEY);
+	if(tuple) {
+    set_text_layer_text(window, tuple->value->cstring);
+  }
+  
+  tuple = dict_find(received, EXIT_KEY);
+	if(tuple) {
+    set_text_layer_text(window, "EXITED FENCE");
+    vibes_short_pulse();
+	}
+}
+
+// Called when an incoming message from PebbleKitJS is dropped
+static void in_dropped_handler(AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+// Called when PebbleKitJS does not acknowledge receipt of a message
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+// Loads up the main window of the app
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+
+  // Create output TextLayer
+  s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
+  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_output_layer, "GEOFENCE");
+  text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
+  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy output TextLayer
+  text_layer_destroy(s_output_layer);
+}
+
+//SINGLE SELECT CLICK
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_output_layer, "Button Clicked!");  
+}
+
+// register click provider 
+void config_provider(Window *window) {
+ // single click 
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+}
+
+
+void init(void) {
+	window = window_create();
+  
+    window_set_window_handlers(window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+	window_stack_push(window, true);
+  window_set_click_config_provider(window, (ClickConfigProvider) config_provider);
+  //window_set_click_config_provider(s_main_window, click_config_provider);
+
+
+	// Register AppMessage handlers
+	app_message_register_inbox_received(in_received_handler); 
+	app_message_register_inbox_dropped(in_dropped_handler); 
+	app_message_register_outbox_failed(out_failed_handler);
+		
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	send_message();
+}
+
+void deinit(void) {
+	app_message_deregister_callbacks();
+	window_destroy(window);
+}
+
+int main( void ) {
+	init();
+	app_event_loop();
+	deinit();
+}
